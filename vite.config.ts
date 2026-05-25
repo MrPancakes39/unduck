44be7dbe5866f4657a40613e476b6b9864575f54
MrPancakes39 <salmannhassan39@gmail.com>
2026-05-25 23:32:52 +0300

    Setup Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
   plugins: [
+    netlify(),
     VitePWA({
       registerType: "autoUpdate",
     }),
